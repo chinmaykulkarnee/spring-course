1da8033c56c0bbb4b8a62af2e510e4fb775864e7
chinmay <chinmay.kulkarni@go-jek.com>
2020-05-30 23:04:29 +0530

    problem

--- src/test/java/com/upgrad/course/AppTest.java
+++ src/test/java/com/upgrad/course/AppTest.java
@@ -1,28 +1,11 @@
 package com.upgrad.course;
 
-import com.upgrad.course.school.Student;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Set;
 
 public class AppTest {
 
     App underTest = new App();
-
-    @Test
-    public void shouldInitializeDivisionBStudents() {
-        Set<Student> students = underTest.getDivisionBStudents();
-        Assert.assertEquals(3, students.size());
-        Assert.assertTrue(students.contains(new Student("Harry", "student1")));
-        Assert.assertTrue(students.contains(new Student("Hermione", "student3")));
-        Assert.assertTrue(students.contains(new Student("Ron", "student2")));
-    }
-
-    @Test
-    public void shouldInitializeDivisionBBenchCount() {
-        Assert.assertEquals(2, underTest.getDivisionBBenchCount());
-    }
 
     @Test
     public void shouldInitializeDivisionBTeacher() {
