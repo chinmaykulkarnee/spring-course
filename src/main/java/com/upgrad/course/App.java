44433521d866a8ef36d53b3100cd349f6643d912
chinmay <chinmay.kulkarni@go-jek.com>
2020-05-30 16:03:13 +0530

    clean slate

--- src/main/java/com/upgrad/course/App.java
+++ src/main/java/com/upgrad/course/App.java
@@ -1,22 +1,7 @@
 package com.upgrad.course;
 
-import org.springframework.context.ApplicationContext;
-
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-    }
-
-    // TODO: change return type of method to return Employee
-    public Object getEmployee() {
-
-        // TODO: initialize ApplicationContext to read bean definitions from beans.xml using ClassPathXmlApplicationContext
-        ApplicationContext ctx = null;
-
-        // TODO: get bean of Employee class using ApplicationContext
-        ctx.getBean(Object.class);
-
-        // TODO: return bean of Employee class
-        return null;
     }
 }
--- src/test/java/com/upgrad/course/AppTest.java
+++ src/test/java/com/upgrad/course/AppTest.java
@@ -1,14 +1,4 @@
 package com.upgrad.course;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class AppTest {
-    App underTest = new App();
-
-    //TODO: uncomment the test once bean has been created properly
-//    @Test
-//    public void shouldInitializeEmployeeBean() {
-//        Assert.assertNotNull(underTest.getEmployee());
-//    }
 }
