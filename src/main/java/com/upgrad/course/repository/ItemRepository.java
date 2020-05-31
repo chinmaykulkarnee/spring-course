de680680d6c2b0298d8673bf6916ab6bb254c430
chinmay <chinmay.kulkarni@go-jek.com>
2020-05-31 23:04:29 +0530

    problem

--- src/main/java/com/upgrad/course/repository/ItemRepository.java
+++ src/main/java/com/upgrad/course/repository/ItemRepository.java
@@ -1,17 +1,11 @@
 package com.upgrad.course.repository;
 
-import com.upgrad.course.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 // TODO: extend JpaRepository with correct parameters for entity and key
-public interface ItemRepository extends JpaRepository<Item, Integer> {
+public interface ItemRepository extends JpaRepository<> {
 
     // TODO: override findAll method to return List instead of Iterable
-    @Override
-    List<Item> findAll();
 
     // TODO: define new method to find items by name
-    List<Item> findByName(String name);
 }
--- src/main/java/com/upgrad/course/repository/OrderRepository.java
+++ src/main/java/com/upgrad/course/repository/OrderRepository.java
@@ -3,14 +3,10 @@ package com.upgrad.course.repository;
 import com.upgrad.course.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     // TODO: override findAll method to return List instead of Iterable
-    @Override
-    List<Order> findAll();
 
     // TODO: define new method to find orders by userId
-    List<Order> findByUserId(String userId);
 }
