939a70e56c1f9c0d6a9b08e2ffbebae5b396ff27
chinmay <chinmay.kulkarni@go-jek.com>
2020-06-01 10:42:51 +0530

    solution updated

--- src/main/java/com/upgrad/course/dao/ProductDao.java
+++ src/main/java/com/upgrad/course/dao/ProductDao.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
@@ -15,11 +16,15 @@ public class ProductDao {
     @PersistenceContext
     private EntityManager em;
 
+    // TODO: use Transactional annotation to allow operation with EntityManager
+    @Transactional
     // TODO: use EntityManager method to save product to database
     public void persist(Product product) {
         em.persist(product);
     }
 
+    // TODO: use Transactional annotation to allow operation with EntityManager
+    @Transactional
     // TODO: use EntityManager createNamedQuery method to get product by name from the database
     public Product find(String name) {
         TypedQuery<Product> query = em.createNamedQuery("Product.findByName", Product.class);
@@ -28,12 +33,15 @@ public class ProductDao {
                 .getSingleResult();
     }
 
+    // TODO: use Transactional annotation to allow operation with EntityManager
+    @Transactional
     // TODO: use EntityManager createNamedQuery method to get products from the database
     public List<Product> findAll() {
         TypedQuery<Product> query = em.createNamedQuery("Product.findAll", Product.class);
         return query.getResultList();
     }
 
+    @Transactional
     public void deleteAll() {
         em.createQuery("DELETE FROM Product")
                 .executeUpdate();
