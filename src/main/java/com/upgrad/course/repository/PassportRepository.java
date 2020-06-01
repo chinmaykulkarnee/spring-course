cc41e1128c461d3daee23f389fedd337c6d46f9c
chinmay <chinmay.kulkarni@go-jek.com>
2020-06-01 10:33:45 +0530

    problem updated

--- src/main/java/com/upgrad/course/repository/PassportRepository.java
+++ src/main/java/com/upgrad/course/repository/PassportRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 // TODO: extend JpaRepository with correct parameters for entity and key
-public interface PassportRepository extends JpaRepository<Passport, Integer> {
+public interface PassportRepository extends JpaRepository<> {
 
     // TODO: override findAll method to return List instead of Iterable
-    @Override
-    List<Passport> findAll();
 
     // TODO: define new method to find passports by number
-    List<Passport> findByNumber(String number);
 }
