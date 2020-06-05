30b09cb75126764fcb4c7a6472b733858a31b0c6
chinmay <chinmay.kulkarni@go-jek.com>
2020-06-05 20:21:49 +0530

    solution updated

--- src/main/java/com/upgrad/course/school/Classroom.java
+++ src/main/java/com/upgrad/course/school/Classroom.java
@@ -2,15 +2,25 @@ package com.upgrad.course.school;
 
 import com.upgrad.course.factory.BenchFactory;
 import com.upgrad.course.factory.StudentFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.Set;
 
 // TODO: mark this class as an auto scan component for bean creation
+@Component
 public class Classroom {
     private StudentFactory studentFactory;
     private BenchFactory benchFactory;
     private Teacher teacher;
 
     // TODO: autowire dependencies using annotation
+    @Autowired
+    public Classroom(StudentFactory studentFactory, BenchFactory benchFactory, Teacher teacher) {
+        this.studentFactory = studentFactory;
+        this.benchFactory = benchFactory;
+        this.teacher = teacher;
+    }
 
     public Set<Student> getStudents() {
         return this.studentFactory.getStudents();
--- src/main/java/com/upgrad/course/school/Teacher.java
+++ src/main/java/com/upgrad/course/school/Teacher.java
@@ -1,6 +1,9 @@
 package com.upgrad.course.school;
 
+import org.springframework.stereotype.Component;
+
 // TODO: mark this class as an auto scan component for bean creation
+@Component
 public class Teacher {
     @Override
     public String toString() {
