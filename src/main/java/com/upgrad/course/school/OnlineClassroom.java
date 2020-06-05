fbf54dc2aa7ba3ed826ded472610341f5a65f561
chinmay <chinmay.kulkarni@go-jek.com>
2020-06-05 20:41:33 +0530

    solution updated

--- src/main/java/com/upgrad/course/school/OnlineClassroom.java
+++ src/main/java/com/upgrad/course/school/OnlineClassroom.java
@@ -1,14 +1,24 @@
 package com.upgrad.course.school;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class OnlineClassroom {
 
     // TODO: Inject course bean with name springCourse using property based autowiring
+    @Autowired
+    @Qualifier("springCourse")
     private Course course;
 
     private Student student;
     private Teacher teacher;
 
     // TODO: Inject student and teacher using constructor based autowiring
+    @Autowired
+    public OnlineClassroom(Student student, Teacher teacher) {
+        this.student = student;
+        this.teacher = teacher;
+    }
 
     public String getStudentDetails() {
         return this.student.getDetails();
--- src/test/java/com/upgrad/course/AppTest.java
+++ src/test/java/com/upgrad/course/AppTest.java
@@ -11,4 +11,14 @@ public class AppTest {
     public void shouldInitializeClassAWithCorrectStudent() {
         Assert.assertEquals("Student [name : Harry Potter id: student1]", underTest.getStudentDetails());
     }
+
+    @Test
+    public void shouldInitializeClassAWithCorrectTeacher() {
+        Assert.assertEquals("Teacher [name : Albus Dumbledore]", underTest.getTeacherDetails());
+    }
+
+    @Test
+    public void shouldInitializeClassAWithCorrectWoodenBench() {
+        Assert.assertEquals("Spring", underTest.getBenchType());
+    }
 }
